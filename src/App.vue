d36df6e684dc3895a02bff9a9a1c96e86375a308
Alireza <alireza.zero.time1994@gmail.com>
2021-06-10 19:32:04 +0430

    add store new item in db and some bugs

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ import SearchBox from "@/components/SearchBox.vue";
 import Layout from "@/layouts/Default.vue";
 import ItemBox from "./components/ItemBox.vue";
 import axios from "axios";
-import debounce from "./utils/debounce";
+// import debounce from "./utils/debounce";
 
 export default {
   name: "App",
@@ -29,49 +29,90 @@ export default {
   data() {
     return {
       items: [],
-      filteredItems: [],
+      dataObject: {},
       searchText: "",
     };
   },
-  watch: {
-    searchText() {
+  watch: {},
+  computed: {
+    filteredItems() {
       if (this.searchText != "") {
-        this.searchInItems();
+        return this.searchInItems();
       } else {
-        this.filteredItems = this.items;
+        return this.items;
       }
     },
   },
   methods: {
+    //fetch items method from server
     fetchItems() {
       axios
         .get("http://localhost:2000/data")
         .then((res) => {
           this.items = Object.values(res.data);
-          this.filteredItems = Object.values(res.data);
+          this.dataObject = res.data;
         })
         .catch((er) => {
           console.error(er);
         });
     },
-    searchInItems: debounce(function() {
-      this.filteredItems = this.items.filter(
-        (item) =>
-          //if we want just title and description has part of search text
 
+    //Search Items Method with debounce
+    // searchInItems: debounce(function() {
+    //   const filterItems = this.items.filter(
+    //     (item) =>
+    //       //if we want just find items where title and description includes search text;
+
+    //       // item.description.includes(this.searchText) ||
+    //       // item.title.text.includes(this.searchText)
+
+    //       //if title or description has exact of searchText
+    //       item.title.text === this.searchText ||
+    //       item.description === this.searchText
+    //   );
+    //   console.log(filterItems);
+    //   return filterItems ? filterItems : [];
+    // }, 500),
+
+    searchInItems() {
+      const filterItems = this.items.filter(
+        (item) =>
+          //if we want just find items where title and description includes search text;
           // item.description.includes(this.searchText) ||
           // item.title.text.includes(this.searchText)
-
           //if title or description has exact of searchText
           item.title.text === this.searchText ||
           item.description === this.searchText
       );
-    }, 500),
+      return filterItems ? filterItems : [];
+    },
+
     onStoreNewItem(itemTitle) {
-      console.log(itemTitle);
+      //Beacuase the data in db.json is not array i must post all data
+      const keys = Object.keys(this.dataObject);
+      const lastKeyValue = keys[keys.length - 1];
+
+      const newItem = {
+        ...this.items[0],
+        title: { text: itemTitle },
+      };
+
+      //Create new key  last key + 1 ; ex lastkey = '9' new key became '10'
+      const newKey = +lastKeyValue + 1;
+      this.dataObject[newKey] = newItem;
+
+      axios
+        .post("http://localhost:2000/data", this.dataObject)
+        .then((res) => {
+          this.items = Object.values(res.data);
+        })
+        .catch((er) => {
+          console.error(er);
+        });
     },
   },
   mounted() {
+    // fetch items from server
     this.fetchItems();
   },
 };
